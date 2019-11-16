2aad39de45c3c465afd65e14c5d75a11b1cb7699
NelinVitaliy <s5001481.nelin@gmail.com>
2019-11-16 14:55:18 +0600

    Chapter_2__Task_4

---
+++ Task_4.py
@@ -0,0 +1,5 @@
+from datetime import timedelta
+first_time = timedelta(hours=6, minutes=1, seconds=30)
+second_time = timedelta(hours=6, minutes=2, seconds=10)
+delta = second_time - first_time
+print(delta)
